7782651586b7b30ee729bedb60456c64cf02646e
lixuanhui337681967 <lixuanhui789@qq.com>
2022-10-21 11:21:34 +0800

    Java每日学习

--- src/com/lxh/process/day_35/Jdbc_04.java
+++ src/com/lxh/process/day_35/Jdbc_04.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
  * @author lixuanhui
  * @date 2022年 10月21日 10:50:20
  */
-public class Jdbc_05 {
+public class Jdbc_04 {
     public static void main(String[] args) throws Exception {
         Connection connection = JdbcUtils.getConnection();
         String sql = "select * from student";
